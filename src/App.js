310ecd12dfd34e438507efff12e00e2c9ea48f3b
Stephen-C-Noh <stephen.c.noh@gmail.com>
2022-03-19 01:09:42 -0400

    6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
     const onClick = () => setValue((prev) => prev + 1);
-    console.log("Called an api.");
+    console.log("I run every time.");
+
+    useEffect(() => {
+        console.log("Calling the API...");
+    }, []);
     return (
         <div>
             <h1>{counter}</h1>
